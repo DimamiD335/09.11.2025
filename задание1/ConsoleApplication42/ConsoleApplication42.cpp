0ee41735fc67ac15e336e04e42efec67209fd211
Moy test <antonovdimamid@gmail.com>
2025-11-10 07:35:47 +0300

    change1

---
+++ задание1/ConsoleApplication42/ConsoleApplication42.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+void add() {
+    int a;
+    int b;
+    std::cout << "Введите первое число: " << std::endl;
+    std::cin >> a;
+    std::cout << "Введите второе число: " << std::endl;
+    std::cin >> b;
+    std::cout << "Результат: " << a + b << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+#ifndef MODE 
+    std::cout << "Mode not initializate" << std::endl;
+#else
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    add();
+#else 
+    std::cout << "Неизвестный режим" << std::endl;
+#endif
+#endif
+
+    return 0;
+}
